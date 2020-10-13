00d5ddf9c9b4cc8567769b396e205176ac684519
Maarten Bakker <maartenbakker@gmail.com>
2020-10-13 17:26:23 +0200

    Implement services as MapService. Closes #5

---
+++ pet-clinic-data/src/main/java/com/maartenmusic/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.maartenmusic.petclinic.services.map;
+
+import com.maartenmusic.petclinic.model.Vet;
+import com.maartenmusic.petclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Long id, Vet object) {
+        return super.save(id, object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
