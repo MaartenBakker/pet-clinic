6367cfc67d1ab7f7d23ce564d5aef866c8200b98
Maarten Bakker <maartenbakker@gmail.com>
2020-10-13 14:43:15 +0200

    Refactored service interfaces to common base interface. Closes #10

--- pet-clinic-data/src/main/java/com/maartenmusic/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/maartenmusic/petclinic/services/VetService.java
@@ -4,13 +4,7 @@ import com.maartenmusic.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends  CrudService<Vet, Long> {
 
 }
 
