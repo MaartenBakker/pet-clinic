c5ea61225f50e570a85f5a6f265d808bad1c3b83
Maarten Bakker <maartenbakker@gmail.com>
2020-10-19 16:09:17 +0200

    Fixed menu links. Closes #44

--- pet-clinic-web/src/main/java/com/maartenmusic/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/maartenmusic/petclinic/controllers/VetController.java
@@ -4,9 +4,7 @@ import com.maartenmusic.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
 @Controller
 public class VetController {
 
@@ -16,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping({"", "/", "/index", "/index.html"})
+    @GetMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
 
