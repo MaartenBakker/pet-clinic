63181c1bb4e565c80538dfc86cc9c10d7607595d
Maarten Bakker <maartenbakker@gmail.com>
2020-10-26 14:06:02 +0100

    Refactoring with Lombok

--- pet-clinic-data/src/main/java/com/maartenmusic/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/maartenmusic/petclinic/model/Visit.java
@@ -1,11 +1,18 @@
 package com.maartenmusic.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 import static javax.persistence.CascadeType.*;
 import static javax.persistence.CascadeType.DETACH;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -19,28 +26,4 @@ public class Visit extends BaseEntity {
     @ManyToOne(cascade = {PERSIST, REFRESH, DETACH})
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
